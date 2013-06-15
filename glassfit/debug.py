02e76915a3359ccae0c41aa3e6f28de3dbe28e4a
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-14 20:08:37 -0400

    debug endpoint

---
+++ glassfit/debug.py
@@ -0,0 +1,22 @@
+import logging
+import webapp2
+import util
+from pprint import pprint
+import json
+
+def show_current_timeline_card(mirror_service, last_x=1):
+    timeline = mirror_service.timeline().list(maxResults=last_x).execute()['items']
+    pprint(timeline)
+    logging.info("Look the current timeline item over here")
+
+class LastCards(webapp2.RequestHandler):
+    """for debug purposes, shows the last X cards in the timeline"""
+    @util.auth_required
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'
+        timeline_items = self.mirror_service.timeline().list().execute()['items']
+        self.response.write(json.dumps(timeline_items))
+
+DEBUG_PATHS = [
+    ('/debug', LastCards)
+]
