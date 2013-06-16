2ea7a525611fdf6dfebc507ca452e2179d576d2b
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-16 13:45:35 -0400

    proper google style datetime formatting

--- glassfit/debug.py
+++ glassfit/debug.py
@@ -3,10 +3,16 @@ from pprint import pprint
 import json
 import webapp2
 import util
+from feed.date import rfc3339
 
 # for now we are using this class for catchall stuff. probably the wrong
 # place for this
 
+def format_timestamp(stamp):
+    """format timestamp according to google"""
+    import re
+    tf = rfc3339.timestamp_from_tf(stamp)
+    return re.sub(r'''-\d\d:\d\d''', '.561Z', tf)
 
 def show_current_timeline_card(mirror_service, last_x=1):
     timeline = mirror_service.timeline().list(maxResults=last_x).execute()['items']
