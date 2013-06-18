e8acf2105410f8003511bc8535f8d102d6876cf6
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-17 20:45:44 -0400

    misc

--- glassfit/main.py
+++ glassfit/main.py
@@ -2,6 +2,8 @@ import logging
 import webapp2
 import util
 
+import glassfit.notify
+
 def start_page_card():
     """Initial card"""
     # TODO probably should some sort of a menu for the user here?
@@ -13,7 +15,6 @@ def start_page_card():
         }
     }
     return timeline_item_body
-
 
 class StartSession(webapp2.RequestHandler):
     """Start a workout session for the user"""
@@ -35,6 +36,10 @@ class StartSession(webapp2.RequestHandler):
 
         self.mirror_service.timeline().insert(body=card).execute()
         self.response.write('User start workout')
+
+        glassfit.notify.NotifyHandler(self, {
+            'payload': 'ready'
+        },None)
 
 START_WORKOUT_PATH = [
     ('/start', StartSession)
