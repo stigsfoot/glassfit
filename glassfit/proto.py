40f0ec4aa3dc9fb85542b39d09d36eb2c3bb7e52
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-19 02:12:04 -0400

    cleaned up task queue driver in proto

--- glassfit/proto.py
+++ glassfit/proto.py
@@ -1,11 +1,7 @@
 import logging
 import util
 import webapp2
-import traceback
-from google.appengine.api import memcache
 from collections import namedtuple
-from glassfit.debug import timestamp_after
-from datetime import datetime
 
 import glassfit.tasks as gtasks
 
@@ -39,51 +35,21 @@ def create_card_body(workout, delivery_time):
         }
     return body
 
-def schedule_workouts(workouts, schedule):
+def schedule_workouts(workouts):
     skip = 0
-    now = datetime.now()
-    ids = []
+    scheduled = []
     for workout in workouts:
-        body = None
-        date_scheduled = timestamp_after(now, 0)
-        if skip == 0:
-            body = create_card_body(workout, None)
-        else:
-            date_scheduled = timestamp_after(now, skip)
-            body = create_card_body(workout, date_scheduled)
-        logging.info("Scheduling card in %s", date_scheduled)
-        card = schedule(body)
+        scheduled.append((create_card_body(workout, None), skip))
         skip += workout.time
-        ids.append(card['id'])
-    return ids
-
-def serialize_ids(ids):
-    return '__'.join([str(x) for x in ids])
-
-def deserialize_ids(ids):
-    return ids.split('__')
-
-def save_workout(userid, workouts_ids):
-    memcache.set(key=userid, value=serialize_ids(workouts_ids), time=3600)
-
-def cancel_workouts(userid, cancel):
-    try:
-        for workout_id in deserialize_ids(memcache.get(key=userid)):
-            cancel(workout_id) # error handling should be done in cancel
-    except Exception:
-        logging.warn('Cancel did not handle exception:')
-        logging.warn(traceback.format_exc())
+    return scheduled
 
 class StartPrototype(webapp2.RequestHandler, gtasks.TaskHandler):
     @util.auth_required
     def get(self):
-        cancel_workouts(self.userid, lambda w_id:
-                self.mirror_service.timeline().delete(id=w_id).execute())
-        workout_ids = schedule_workouts(workouts,
-                lambda body: self.mirror_service.timeline() \
-                    .insert(body=body).execute())
-        logging.info("Scheduled workouts: %s", str(workout_ids))
-        save_workout(self.userid, workout_ids)
+        scheduled_cards = schedule_workouts(workouts)
+        self.send_cards(self.userid, scheduled_cards)
+        logging.info("Scheduled workouts: %s", str(scheduled_cards))
+
 
 PROTOTYPE_PATH = [
     ('/proto', StartPrototype)
