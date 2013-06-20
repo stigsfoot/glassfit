de549dabfe270fd198d7b7180569be2f4e89c279
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-20 00:13:47 -0400

    removed unused code

--- glassfit/proto.py
+++ glassfit/proto.py
@@ -4,6 +4,7 @@ import webapp2
 from collections import namedtuple
 
 import glassfit.tasks as gtasks
+import glassfit.workout as gworkout
 
 Exercise = namedtuple('Exercise', ['name', 'time'])
 
@@ -13,42 +14,19 @@ workouts = [
     Exercise(name='jumpingjacks', time=10)
 ]
 
-def create_card_body(workout, delivery_time):
-    body = {
-        'text': 'Working out - doing {w} for {s} seconds'
-            .format(w=workout.name, s=workout.time),
-        'menuItems':[{
-            'action': 'CUSTOM',
-            'id': 'cancel',
-            'values': [{
-                'displayName': 'Cancel!',
-                'iconUrl': 'http://i.imgur.com/dkvdDby.png'
-            }],
-        }],
-    }
-    if delivery_time is not None:
-        body['notification'] = {
-            # displayTime/deliveryTime is the other thing we can try
-            #'displayTime': delivery_time,
-            'deliveryTime': delivery_time,
-            'level': 'DEFAULT'
-        }
-    return body
-
 def schedule_workouts(workouts):
     skip = 0
     scheduled = []
     for workout in workouts:
-        scheduled.append((create_card_body(workout, None), skip))
+        scheduled.append(workout, skip)
         skip += workout.time
     return scheduled
 
 class StartPrototype(webapp2.RequestHandler, gtasks.TaskHandler):
     @util.auth_required
     def get(self):
-        scheduled_cards = schedule_workouts(workouts)
+        scheduled_cards = schedule_workouts(gworkout.workout)
         self.send_cards(self.userid, scheduled_cards)
         logging.info("Scheduled workouts: %s", str(scheduled_cards))
-
 
 PROTOTYPE_PATH = [ ('/proto', StartPrototype) ]
